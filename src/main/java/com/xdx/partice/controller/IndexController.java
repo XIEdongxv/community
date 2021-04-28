9cb2819907df3d780cfbaf9789a4e995cb46c2b1
XIEdongxv <3102387915@qq.com>
2021-04-28 13:57:32 +0800

    add index and bootstrap

---
+++ src/main/java/com/xdx/partice/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.xdx.partice.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author xdxstart
+ * @create 2021-04-28 9:15
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+    return "index";
+    }
+}
